eb2236c29993b5c752d777b12c9d3fde5221bc63
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-10-30 17:39:43 +0700

    package controller added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const restaurantRoute = require("./routes/restaurant-route");
 const chatRoute = require("./routes/chat-route");
 const notFoundMiddleware = require("./middleware/notFoundMiddleware");
 const errorMw = require("./middleware/errorMiddleware");
-const authRoute = require('./routes/auth-routhes')
+const authRoute = require("./routes/auth-routhes");
+const packageRoute = require("./routes/package-route");
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -16,8 +17,8 @@ app.use(express.json());
 
 app.use("/restaurant", restaurantRoute);
 app.use("/chat", chatRoute);
-
-app.use('/auth', authRoute)
+app.use("/package", packageRoute);
+app.use("/auth", authRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMw);
--- src/routes/package-route.js
+++ src/routes/package-route.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
-router.post();
+const packageController = require("../controllers/package-controller");
+
+router.post("/create", upload.single(), packageController.createPackage);
 
 module.exports = router;
