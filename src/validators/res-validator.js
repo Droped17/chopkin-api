ba3f3aa4d049cb52ed5dbac5117924e78712d12e
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-10-30 16:07:38 +0700

    added more res controllers and fixed schema and added readme

--- src/validators/res-validator.js
+++ src/validators/res-validator.js
@@ -3,3 +3,7 @@ const Joi = require("joi");
 exports.resIdSchema = Joi.object({
   resId: Joi.number().integer().positive().required(),
 });
+
+exports.resNationSchema = Joi.object({
+  nation: Joi.string().trim().required(),
+});
