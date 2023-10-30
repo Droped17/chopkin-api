77158fee0234c7baa647d70acc3fc707f2440859
Yuji Hosaka <h.kyouiku58@gmail.com>
2023-10-30 14:19:52 +0700

    auth-validator, create-error

---
+++ src/validators/auth-validator.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+  firstName: Joi.string().trim().required(),
+  lastName: Joi.string().trim().required(),
+  emailOrMobile: Joi.alternatives([
+    Joi.string().email(),
+    Joi.string().pattern(/^[0-9]{10}$/),
+  ])
+    .required()
+    .strip(),
+  mobile: Joi.string()
+    .pattern(/^[0-9]{10}$/)
+    .required(),
+  email: Joi.string().email().trim().required(),
+  password: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{6,30}$/)
+    .trim()
+    .required(),
+  confirmPassword: Joi.string()
+    .valid(Joi.ref("password"))
+    .trim()
+    .required()
+    .strip(),
+});
+
+exports.registerSchema = registerSchema;
+
+const loginSchema = Joi.object({
+  emailOrMobile: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+exports.loginSchema = loginSchema;
