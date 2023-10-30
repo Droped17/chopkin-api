df198b9543ad3bce56c864b5ec41fc1dfcd4291e
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-10-30 12:03:31 +0700

    added restaurant route

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 
+const restaurantRoute = require("./routes/restaurant-route");
+
 app.use(cors());
 app.use(morgan());
 app.use(express.json());
+
+app.use(restaurantRoute);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log("server is running on port", port));
