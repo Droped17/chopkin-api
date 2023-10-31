6c5953e547f26e2986bdbb35ab7c8b4b1d378027
janjao937 <janjao937@gmail.com>
2023-10-31 18:02:18 +0700

    customer login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -185,92 +185,86 @@ const CustomerRegister = async (req, res, next) => {
   }
 }
 
+const login = async(req,res,next)=>{
+  try{
+    const data = req.body;
+    const checkRestaurants = await prisma.restaurant.findFirst({
+      where: {
+        OR: [{ email: data.email }, { phone: data.phone }],
+      }
+    });
+    const checkCustomer = await prisma.customer.findFirst({
+      where: {
+        OR: [{ email: data.email }, { phone: data.phone }],
+      }
+    });
+
+    const checkAdmin = await prisma.admin.findFirst({
+      where:{
+        email:data.email
+      }
+    });
+
+    if(checkRestaurants)restaurantLogin(req,res,next);
+    else if(checkCustomer)customerLogin(req,res,next);
+    else if(checkAdmin)adminLogin(req,res,next);
+    else res.status(404).json({message:"not found this user"});
+  }
+  catch(error){
+    next(error);
+  }
+
+}
+
+const customerLogin = async(req,res,next)=>{
+  try{
+    const data = req.body;
+    const customer = await prisma.customer.findFirst({
+       where: {
+        OR: [{ email: data.email }, { phone: data.phone }],
+      }
+    });
+
+    const passwordCheck = await bcrypt.compare(data.password,customer.password);
+
+    if(!passwordCheck){
+      return next(createError("password not match", 400));
+    }
+    //payload 
+    const payload = { customerId: customer.id };
+    //accessToken
+    const accessToken = jwt.sign(payload,process.JWT_SECRET_KEY || "8JncnNqEPncnca7ranc47anda",{ expiresIn: process.env.JWT_EXPIRE });
+
+    res.status(200).json({message:"customer Login",accessToken,customer});
+
+  }catch(error)
+  {
+    next(error);
+  }
+}
 
 
-// exports.register = async (req, res, next) => {
-//   try {
-//     const { value, error } = registerSchema.validate(req.body);
-//     if (error) {
-//       return next(error);
-//     }
-//     value.password = await bcrypt.hash(value.password, 12);
-//     const customer = await prisma.customer.create({ data: value });
-//     const payload = { CustomerId: Customer.id };
-//     const accessToken = jwt.sign(
-//       payload,
-//       process.env.JWT_SECRET_KEY || "8JncnNqEPncnca7ranc47anda",
-//       {
-//         expiresIn: process.env.JWT_EXPIRE,
-//       }
-//     );
-//     res.status(201).json({ accessToken });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+const restaurantLogin = async(req,res,next)=>{
+  try{
+    
 
-// exports.login = async (req, res, next) => {
-//   try {
-//     const { value, error } = loginSchema.validate(req.body);
-//     if (error) {
-//       console.log(error.name);
-//       return next(error);
-//     }
-//     const customer = await prisma.customer.findFirst({
-//       where: {
-//         OR: [{ email: value.email }, { phone: value.phone }],
-//       },
-//     });
-//     if (!customer) {
-//       return next(createError("invalid credential", 400));
-//     }
+  }catch(error)
+  {
+    next(error);
+  }
+}
+const adminLogin = async(req,res,next)=>{
+  try{
+    
 
-//     const isMatch = await bcrypt.compare(value.password, customer.password);
-//     if (!isMatch) {
-//       return next(createError("invalid credential", 400));
-//     }
-//     const payload = { CustomerId: customer.id };
-//     const accessToken = jwt.sign(
-//       payload,
-//       process.env.JWT_SECRET_KEY || "8ICNd310ncCXaldnenq",
-//       { expiresIn: process.env.JWT_EXPIRE }
-//     );
-//     res.status(200).json({ accessToken });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+  }catch(error)
+  {
+    next(error);
+  }
+}
+
+
 
 exports.register = register;
 exports.createAdmin = createAdmin;
-// exports.login = async (req, res, next) => {
-//   try {
-//     const { value, error } = loginSchema.validate(req.body);
-//     if (error) {
-//       console.log(error.name);
-//       return next(error);
-//     }
-//     const customer = await prisma.customer.findFirst({
-//       where: {
-//         OR: [{ email: value.email }, { phone: value.phone }],
-//       },
-//     });
-//     if (!customer) {
-//       return next(createError("invalid credential", 400));
-//     }
-
-//     const isMatch = await bcrypt.compare(value.password, customer.password);
-//     if (!isMatch) {
-//       return next(createError("invalid credential", 400));
-//     }
-//     const payload = { CustomerId: customer.id };
-//     const accessToken = jwt.sign(
-//       payload,
-//       process.env.JWT_SECRET_KEY || "8ICNd310ncCXaldnenq",
-//       { expiresIn: process.env.JWT_EXPIRE }
-//     );
-//     res.status(200).json({ accessToken });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+exports.login = login;
