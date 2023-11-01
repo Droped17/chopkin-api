486d9dee1e2d31f7301a32bee22705d81a95ab00
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-11-01 17:59:29 +0700

    added params for res route

--- src/routes/restaurant-route.js
+++ src/routes/restaurant-route.js
@@ -8,10 +8,10 @@ router.get("/:resId", resController.getResById); // GET RESTAURANT BY ID FOR RES
 router.get("/getPendingRes", resController.getPendingRes); // GET ALL PENDING RESTAURANTS
 router.get("/:nationIndex", resController.getResByNation); // GET RESTAURANTS BY NATIONALITY
 router.get("/:catIndex", resController.getResByCat); // GET RESTAURANTS BY CATEGORY
-router.delete("/delete", resController.deleteRes); // DELETE RESTAURANT
-router.patch("/edit", resController.createEditPending); // CREATE EDIT PENDING
+router.delete("/delete/:resId", resController.deleteRes); // DELETE RESTAURANT
+router.patch("/edit/:resId", resController.createEditPending); // CREATE EDIT PENDING
 router.get("/getPendingEdit", resController.getEditPending); // GET ALL EDIT PENDINGS
-router.delete("/editPending", resController.deleteEditPending); // DELETE EDIT PENDING
-router.patch("/updateStatus", resController.updateResStatus); // UPDATE RESTAURANT STATUS
+router.delete("/editPending/:resId", resController.deleteEditPending); // DELETE EDIT PENDING
+router.patch("/updateStatus/:resId", resController.updateResStatus); // UPDATE RESTAURANT STATUS
 
 module.exports = router;
