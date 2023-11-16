e63cf46035a40570afbf9ad1c122fa6c07ee6cd2
janjao937 <janjao937@gmail.com>
2023-11-16 14:41:41 +0700

    chat chatController

--- src/controllers/chat-controller.js
+++ src/controllers/chat-controller.js
@@ -7,30 +7,58 @@ require("dotenv");
 //   }
 
 
+const GetAllRoom = async(req,res,next)=>{
+    const allRoom = await prisma.chatRoom.findMany({});
+
+    res.status(200).json({message:"all resturant",allRoom});
+}
+
 const UserCreateRoom = async(req,res,next)=>{
     try{
         const roomId = req.body.roomId;
 
-        await prisma.chatRoom.create({
-            data:{
-                
+        //validate
+        const isHaveThisRoomId = prisma.chatRoom.findFirst({
+            where:{
+                roomId:roomId
             }
         })
 
+        if(isHaveThisRoomId) return next(createError("have this roomId:"+isHaveThisRoomId.roomId+"on database",405));
+
+        const newRoom = await prisma.chatRoom.create({
+            data:{
+                roomId:roomId
+            }
+        });
+
+        res.status(200).json({message:"create room success||RoomId:"+newRoom.roomId});
+
     }
     catch(error){
         next(error);
     }
 }
 
-const DeleteRoom = async(req,res,next)=>{
+const DeleteRoomById = async(req,res,next)=>{
     try{
+        const roomId = req.params.roomId;
+        const isHaveThisRoomId = await prisma.chatRoom.findFirst({
+            where:{
+                roomId:roomId
+            }
+        });
+
+        if(!isHaveThisRoomId)return res
 
     }
     catch(error){
         next(error);
     }
 }
+
+exports.UserCreateRoom = UserCreateRoom;
+exports.DeleteRoomById = DeleteRoomById;
 
 
 
--- src/routes/chat-route.js
+++ src/routes/chat-route.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const {UserCreateRoom,DeleteRoomById} = require("../controllers/chat-controller");
+
+router.post("",UserCreateRoom);
+router.delete("",DeleteRoomById);
 
 module.exports = router;
