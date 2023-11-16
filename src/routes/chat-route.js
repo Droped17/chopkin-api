de573adeb337dfa955a6574f6b38cd559f639685
auttpon <auttaponcoe24@gmail.com>
2023-11-16 19:24:42 +0700

    roomAll

--- src/routes/chat-route.js
+++ src/routes/chat-route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {UserCreateRoom,DeleteRoomById,GetAllRoom} = require("../controllers/chat-controller");
+const {
+	UserCreateRoom,
+	DeleteRoomById,
+	GetAllRoom,
+} = require("../controllers/chat-controller");
 
 // chat/roomId:
 
-router.get("/room/:roomId",GetAllRoom);
-router.post("/create",UserCreateRoom);
-router.delete("/delete/:roomId",DeleteRoomById);
+router.get("/roomAll", GetAllRoom);
+router.post("/create", UserCreateRoom);
+router.delete("/delete/:roomId", DeleteRoomById);
 
 module.exports = router;
